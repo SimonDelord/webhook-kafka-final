76a45a9760dd52f7d521b6b9d3928d73df77d264
SimonDelord <simon.delord@gmail.com>
2024-01-18 16:55:04 +1100

    Update webhook-kafka.py

--- webhook-kafka.py
+++ webhook-kafka.py
@@ -1,29 +1,18 @@
-from flask import Flask, Response, request
-from flask_cors import CORS
+from flask import Flask, request, jsonify
 from kafka import KafkaProducer
-import json
 
-#producer = KafkaProducer(bootstrap_servers='host:port',value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-
-#Initialize the Flask app
 app = Flask(__name__)
-#VERY UNSAFE for production. 
-#Allows flask to accept cross-origin requests for local development
-CORS(app)
-
-#Register a function @ / that could be GET or POST
-# defaults to http://localhost:5000/
-@app.route("/", methods=["GET","POST"])
-def hello(): #hello() is registered to route /
-    #Gets the POST body as a JSON object
-    print request.get_json()
-    body = request.get_json()
+@app.route('/webhook', methods=['POST'])
+def webhook_receiver():
+    # Process the data and perform actions based on the event
+    body = request.json
+    print("Received webhook data:", body)
+    return jsonify({'message': 'Webhook received successfully'}), 200
 
     #Bootstraps an instance of a Kafka producer.
     #Initializes the producer and identifies the docker server.
-    #kafka-spotify is listed in /etc/hosts with the ip of the container
     #Sets the producer serializer to JSON
-    producer = KafkaProducer(bootstrap_servers='kafka-broker-kafka-bootstrap:9092',value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+    producer = KafkaProducer(bootstrap_servers='my-cluster-kafka-bootstrap:9092',value_serializer=lambda v: json.dumps(v).encode('utf-8'))
     #Send a message to the kafka topic 'acs-topic'
     #passes the POST JSON body as the message
     producer.send('acs-topic', body)
@@ -31,9 +20,6 @@ def hello(): #hello() is registered to route /
     producer.close()
     #Returns a Complete string
     return "Complete"
-
-#Main process function
-if __name__ == "__main__":
-    #Bind to broadcast so you can access the server
-    #app.run('0.0.0.0')
+if __name__ == '__main__':
+#    app.run(debug=True)
     app.run(debug=True,host='0.0.0.0',port=8001)
